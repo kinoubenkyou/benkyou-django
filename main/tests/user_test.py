1caddcd6baf9a5ee5524c8a6cc528b59eafb6b6a
kinoubenkyou <kinoubenkyou@gmail.com>
2024-07-10 14:08:37 +0700

    Refactor

--- main/tests/user_test.py
+++ main/tests/user_test.py
@@ -1,12 +1,13 @@
-from unittest import TestCase
+from django.test import TestCase
 
 from main.models import User
 
 
 class UserTest(TestCase):
+    fixtures = ['user']
+
     def test_get_absolute_url(self):
-        object_ = User()
-        object_.pk = 1
+        object_ = User.objects.get(pk=1)
 
         actual = object_.get_absolute_url()
 
