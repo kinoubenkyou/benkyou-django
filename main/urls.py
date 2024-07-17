c48b7c0d694ecd245ba9d7cacb0375bde61257d5
kinoubenkyou <kinoubenkyou@gmail.com>
2024-07-17 14:45:40 +0700

    Refactor

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ from main.views import UserListView, UserDetailView, UserCreateView, UserUpdateV
 urlpatterns = [
     path('users/', UserListView.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-    path('users/<int:pk>/delete', UserDeleteView.as_view(), name='user-delete'),
-    path('users/<int:pk>/update', UserUpdateView.as_view(), name='user-update'),
+    path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='user-delete'),
+    path('users/<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
     path('users/create/', UserCreateView.as_view(), name='user-create'),
 ]
