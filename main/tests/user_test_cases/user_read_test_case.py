0e1a511e1d75abb7fd05dfaf87e953cc2c40187f
kinoubenkyou <kinoubenkyou@gmail.com>
2024-08-30 17:10:30 +0700

    Refactor

--- main/tests/user_test_cases/user_read_test_case.py
+++ main/tests/user_test_cases/user_read_test_case.py
@@ -4,8 +4,8 @@ from main.tests.test_case import TestCase
 
 class UserReadTestCase(LoginMixin, TestCase):
     def test_success(self):
-        self.login()
         self.web_driver.get(f"{self.live_server_url}/user/")
+        self.login()
 
         self.assertEqual(len(self.find_elements("Email: email@email.com")), 1)
         self.assertEqual(len(self.find_elements("Name: name")), 1)
--- main/tests/user_test_cases/user_sign_in_test_case.py
+++ main/tests/user_test_cases/user_sign_in_test_case.py
@@ -4,6 +4,7 @@ from main.tests.test_case import TestCase
 
 class UserSignInTestCase(LoginMixin, TestCase):
     def test_success(self):
+        self.web_driver.get(f"{self.live_server_url}/user/sign_in/")
         self.login()
 
         self.assertEqual(len(self.find_elements("Signed in.")), 1)
