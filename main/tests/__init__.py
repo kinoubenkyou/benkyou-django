2db386ffe1e3bd343307e8121afb9614e707e6e7
kinoubenkyou <kinoubenkyou@gmail.com>
2024-09-01 15:58:21 +0700

    User verify email (#13)

--- main/tests/__init__.py
+++ main/tests/__init__.py
@@ -1,6 +1,15 @@
-__all__ = ["TestCase", "UserReadTestCase", "UserCreateTestCase", "UserSignInTestCase"]
+__all__ = [
+    "TestCase",
+    "UserReadTestCase",
+    "UserCreateTestCase",
+    "UserSignInTestCase",
+    "UserVerifyEmailTestCase",
+]
 
 from main.tests.test_case import TestCase
 from main.tests.user_test_cases.user_create_test_case import UserCreateTestCase
 from main.tests.user_test_cases.user_read_test_case import UserReadTestCase
 from main.tests.user_test_cases.user_sign_in_test_case import UserSignInTestCase
+from main.tests.user_test_cases.user_verify_email_test_case import (
+    UserVerifyEmailTestCase,
+)
