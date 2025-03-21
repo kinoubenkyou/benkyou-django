b06a14e4f80db98e794e467a1621682856479c8a
kinoubenkyou <kinoubenkyou@gmail.com>
2025-03-21 11:44:28 +0700

    fix deploy not have drf_spectacular package (#39)

--- benkyou_django/urls.py
+++ benkyou_django/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -6,7 +7,6 @@ from drf_spectacular.views import (
     SpectacularRedocView,
 )
 
-from benkyou_django.settings import DEBUG
 from main.routers import SingleResourceRouter
 from main.views.api.user import UserTokenApiView
 from main.views.user import (
@@ -25,12 +25,6 @@ router.register(r"user", UserViewSet, basename="api-user")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
-    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
-    path(
-        "api/schema/redoc/",
-        SpectacularRedocView.as_view(url_name="api-schema"),
-        name="api-schema-redoc",
-    ),
     path("api/user/token/", UserTokenApiView.as_view(), name="api-user-token"),
     path("user/create/", UserCreateView.as_view(), name="user-create"),
     path("user/delete/", UserDeleteView.as_view(), name="user-delete"),
@@ -40,7 +34,7 @@ urlpatterns = [
     path("user/sign_out/", UserSignOutView.as_view(), name="user-sign-out"),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
         path(
