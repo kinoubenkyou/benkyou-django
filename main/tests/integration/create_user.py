1d20a4b98d1147fb2f9f82bb65b1b8ccad695a49
kinoubenkyou <kinoubenkyou@gmail.com>
2025-04-02 14:45:42 +0700

    refactor (#45)
    * add EMAIL_USE_TLS environment variable
    
    * edit setting file
    
    * edit README file
    
    * change to dark scheme
    
    * edit create user test case

--- main/tests/integration/create_user.py
+++ main/tests/integration/create_user.py
@@ -39,7 +39,7 @@ class CreateUserTestCase(SeleniumTestCase):
         sentinel = object()
         token = cache.get(f"verify_user_email.{user.pk}", sentinel)  # type: ignore[union-attr]
         self.assertIsNot(token, sentinel)
-        self.assertIn(
+        self.assertEqual(
             f"{self.live_server_url}/user/verify_email?{urlencode({'token': token})}",
             mail.outbox[0].body,
         )
