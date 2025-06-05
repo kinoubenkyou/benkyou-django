88a562a6dfa58e48e260de32792e7fdd3bd3c116
kinoubenkyou <kinoubenkyou@gmail.com>
2025-06-05 18:02:25 +0700

    add update user password api feature (#59)

---
+++ main/serializers/user/update_password.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import (
+    password_validators_help_text_html,
+    validate_password,
+)
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import CharField
+from rest_framework.serializers import Serializer
+
+
+class UserUpdatePasswordSerializer(Serializer[None]):
+    old_password = CharField()
+    new_password = CharField(
+        help_text=password_validators_help_text_html(),
+        write_only=True,
+        validators=(validate_password,),
+    )
+
+    def validate_old_password(self, value: str) -> str:
+        """Validate the old password."""
+        request = self.context.get("request")
+        user = authenticate(
+            request=request,
+            username=request.user.username,  # type: ignore[union-attr]
+            password=value,
+        )
+        if not user:
+            raise ValidationError("incorrect old password")
+        return value
