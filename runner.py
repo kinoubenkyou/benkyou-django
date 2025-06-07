4fd3642d951673c4583cfeb9cb90d2358b1a4877
kinoubenkyou <kinoubenkyou@gmail.com>
2025-06-07 10:12:12 +0700

    rename return_

--- runner.py
+++ runner.py
@@ -7,9 +7,8 @@ from django.test.runner import DiscoverRunner as DjangoDiscoverRunner
 class DiscoverRunner(DjangoDiscoverRunner):
     def setup_test_environment(self, **kwargs: Any) -> None:
         """Set cache to another Redis logical database."""
-        return_ = super().setup_test_environment(**kwargs)
+        super().setup_test_environment(**kwargs)
         settings.CACHES["default"]["BACKEND"] = (
             "django.core.cache.backends.locmem.LocMemCache"
         )
         settings.CELERY_TASK_ALWAYS_EAGER = True
-        return return_
